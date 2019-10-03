15132c718da9cdb3907d4fc791bb3e8aab4513ed
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-03 10:37:04 -0500

    constructor pattern

--- base/index.js
+++ base/index.js
@@ -6,6 +6,15 @@ class Car {
   }
 }
 
+class Suv extends Car {
+  constructor(doors, engine, color) {
+    super(doors, engine, color);
+    this.wheels = 4;
+  }
+}
+
 const civic = new Car(4, "V6", "grey");
+const cx5 = new Suv(4, "V8", "red");
 
 console.log(civic);
+console.log(cx5);
