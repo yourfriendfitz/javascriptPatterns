11fc6dd1f174e04ef1ab55e9730da9fe8efc7c7f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-03 11:23:26 -0500

    observer pattern

--- base/index.js
+++ base/index.js
@@ -1,40 +1,32 @@
 class Car {
-  constructor(doors, engine, color) {
-    this.doors = doors;
-    this.engine = engine;
-    this.color = color;
-  }
-}
-
-class CarFactory {
-  createCar(type) {
-    switch (type) {
-      case "civic":
-        return new Car(4, "V6", "grey");
-      case "honda":
-        return new Car(2, "V4", "red");
+    constructor(gas) {
+        this.gas = gas;
     }
-  }
+
+    setGasLevel(val) {
+        this.gas = val;
+        this.notifyAll();
+    }
+
+    register(observer) {
+        this.actions.push(observer);
+    }
+
+    unregister(observer) {
+        this.actions.remove.filter(function(el) {
+            return el !== observer;
+        });
+    }
+
+    notifyAll() {
+        return this.actions.forEach(function(el) {
+            el.update(this);
+        }.bind(this));
+    }
 }
 
-let carMixin = {
-  revEngine() {
-    console.log(`The ${this.engine} is doing Vroom Vroom`);
-  }
-};
-
-const carFactory = new CarFactory();
-
-const autoManufacturer = (type, model) => {
-  switch (type) {
-    case "car":
-      return carFactory.createCar(model);
-  }
-};
-
-Object.assign(Car.prototype, carMixin);
-
-const civic = autoManufacturer("car", "civic");
-
-console.log(civic);
-civic.revEngine();
+class consumption {
+    update(car) {
+        car.gas = car.gas + 1;
+    }
+}
