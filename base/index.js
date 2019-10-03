e556bb23e016a9d411f74a29e37f042c2ca67cdd
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-10-03 10:41:11 -0500

    singleton pattern

--- base/index.js
+++ base/index.js
@@ -1,20 +1,20 @@
+let instance = null;
+
 class Car {
   constructor(doors, engine, color) {
-    this.doors = doors;
-    this.engine = engine;
-    this.color = color;
-  }
-}
-
-class Suv extends Car {
-  constructor(doors, engine, color) {
-    super(doors, engine, color);
-    this.wheels = 4;
+    if (!instance) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;
+      instance = this;
+    } else {
+      return instance;
+    }
   }
 }
 
 const civic = new Car(4, "V6", "grey");
-const cx5 = new Suv(4, "V8", "red");
+const honda = new Car(2, "V4", "red");
 
 console.log(civic);
-console.log(cx5);
+console.log(honda);
